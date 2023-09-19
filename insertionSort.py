df285c8cfa763906c7e89c10cf454cad77cd1c18
mehruddin11 <mehruddinkhan@mehruddins-MacBook-Air.local>
2023-09-19 23:34:35 +0530

    changes

---
+++ insertionSort.py
@@ -0,0 +1,10 @@
+def inserttionSort( arr):
+    for j in range(1 ,len(arr)):
+        key = arr[j]
+        i= j-1
+        while(i >= 0 and arr[i] > key):
+            arr[i+1]= arr[i]
+            i-=1
+        arr[i+1]= key
+    return arr
+print(inserttionSort([6,4,3,1]))
