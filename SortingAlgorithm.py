f0d633cf1055b279d8a82201ddc2a9bb635d2ddb
mehruddin11 <mehruddinkhan@mehruddins-MacBook-Air.local>
2023-09-20 23:44:24 +0530

    quick sort

--- SortingAlgorithm.py
+++ SortingAlgorithm.py
@@ -9,6 +9,7 @@ class SortingAlgorithm:
                 i -= 1
             arr[i + 1] = key
         return arr
+
     # merge sort 
     def MergeSort(self, a, left, right):
         if left < right:
@@ -16,16 +17,15 @@ class SortingAlgorithm:
             self.MergeSort(a, left, mid)
             self.MergeSort(a, mid + 1, right)
             self.merge(a, left, mid, right)
+            
     # two way merging 
     def merge(self, a, left, mid, right):
         temp1, temp2 = [], []
         ans = []
         for i in range(left, mid + 1):
             temp1.append(a[i])
-
         for j in range(mid + 1, right + 1):
             temp2.append(a[j])
-
         p1 = 0
         p2 = 0
         while p1 < len(temp1) and p2 < len(temp2):
@@ -41,17 +41,42 @@ class SortingAlgorithm:
         while p2 < len(temp2):
             ans.append(temp2[p2])
             p2 += 1
-
-        # Update the original array a with the sorted result
         for i in range(len(ans)):
             a[left + i] = ans[i]
+    
+    # quick sort 
+    def quickSort(self, A, l, h):
+        if l < h:
+            j = self.partition(A, l, h)
+            self.quickSort(A, l, j)
+            self.quickSort(A, j + 1, h)
+    def partition(self, A, l, h):
+        pivot = A[l]
+        i, j = l, h
+        while True:
+            while A[i] < pivot:
+                i += 1
+            while A[j] > pivot:
+                j -= 1
+            if i >= j:
+                return j
+            A[i], A[j] = A[j], A[i]
 
 sol = SortingAlgorithm()
+# insertion sort
 arr = [1, 29, 0]
 print(sol.insertionSort(arr))
 
+# merge sort
 arr = [1, 8, 6, 3, 9, 8]
 left = 0
-right = len(arr) - 1  # Subtract 1 to get the correct right index
+right = len(arr) - 1  
 sol.MergeSort(arr, left, right)
-print(arr)  # Print the sorted array
+print(arr) 
+
+# quick sort
+qrr=[1,8,4,2,6,0]
+l= 0
+r= len(qrr)-1
+print(sol.quickSort(qrr,l,r))
+print("arr",qrr)
